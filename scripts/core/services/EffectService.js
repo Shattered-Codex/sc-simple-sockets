fb559dc3ecaf80c7c968952334bb5c37974c0276
Gustavo <gubacelar@gmail.com>
2026-05-02 15:46:55 -0300

    fix: apply socketed gem enchantments as item enchantments (#70)
    fix: apply socketed gem enchantments as item enchantments (#70)

--- scripts/core/services/EffectService.js
+++ scripts/core/services/EffectService.js
@@ -14,12 +14,22 @@ export class EffectService {
         Constants.localize("SCSockets.Effects.DefaultName", "Gem Effect");
       data.img ??= eff.img ?? gemItem.img;
       data.disabled = false;
-      data.transfer = true;
-      data.origin = hostItem.uuid;
+      if (EffectService.#isEnchantmentEffect(data)) {
+        data.transfer = false;
+        data.origin = EffectService.#getAppliedEnchantmentOrigin(data, eff, gemItem, hostItem);
+        data.flags ??= {};
+        data.flags.dnd5e ??= {};
+        data.flags.dnd5e.enchantmentProfile ??= eff.id;
+      } else {
+        data.transfer = true;
+        data.origin = hostItem.uuid;
+      }
       data.flags ??= {};
       data.flags[Constants.MODULE_ID] ??= {};
       data.flags[Constants.MODULE_ID][Constants.FLAG_SOURCE_GEM] = {
-        slot: slotIndex
+        slot: slotIndex,
+        sourceId: eff.id,
+        type: data.type ?? "base"
       };
       return data;
     });
@@ -34,5 +44,17 @@ export class EffectService {
     if (ids.length) {
       await hostItem.deleteEmbeddedDocuments("ActiveEffect", ids, options);
     }
+  }
+
+  static #isEnchantmentEffect(effectData) {
+    return effectData?.type === "enchantment" || effectData?.flags?.dnd5e?.type === "enchantment";
+  }
+
+  static #getAppliedEnchantmentOrigin(effectData, effect, gemItem, hostItem) {
+    const origin = effectData?.origin;
+    if (origin && origin !== hostItem?.uuid) {
+      return origin;
+    }
+    return effect?.uuid ?? gemItem?.uuid ?? origin;
   }
 }
